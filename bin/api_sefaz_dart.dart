575f8ab8c3ed850ffc38ca17178e67e2aa04a88e
Jonas <alves.souza.jonas@gmail.com>
2022-07-11 21:38:40 -0300

    first commit

---
+++ bin/api_sefaz_dart.dart
@@ -0,0 +1,5 @@
+import 'package:api_sefaz_dart/api_sefaz_dart.dart' as api_sefaz_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${api_sefaz_dart.calculate()}!');
+}
---
+++ test/api_sefaz_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:api_sefaz_dart/api_sefaz_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
